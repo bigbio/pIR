ef0d1511d217b520233d7f6bfa871c5963865bcd
Yasset Perez-Riverol <ypriverol@gmail.com>
2015-06-09 10:24:24 +0100

    minor changes

---
+++ R/pIBenchmark.R
@@ -0,0 +1,69 @@
+#' Isoelectric poit of a set of peptides from a published paper
+#'
+#' This dataset contains more than 6000 peptides with the experimental isoelectric point and the predicted pI using different methods.
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item SEQUENCE. The potein accession
+#'   \item EXPERIMENTAL. The experimental isoelectric point
+#'   \item COFACTOR. The isoelectric point using the method developed by Cargile and co-workers.
+#'   \item ITERATIVE_THURLKILL
+#'   \item ITERATIVE_TOSELAND
+#'   \item ITERATIVE_GRIMSLEY
+#'   \item ITERATIVE_SOLOMON
+#'   \item ITERATIVE_EMBOSS
+#'   \item ITERATIVE_LEHNINGER
+#'   \item ITERATIVE_PATRICKIOS
+#'   \item ITERATIVE_RICHARD
+#'   \item ITERATIVE_RODWELL
+#'   \item ITERATIVE_SILLERO
+#'   \item BRANCA
+#'   \item BJELL_DEFAULT
+#'   \item BJELL_SKOOG
+#'   \item BJELL_CALLIBRATED
+#'   \item BJELL_EXPASY
+#'   \item SVM
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name peptides
+#' @usage data(peptides)
+#' @format A data frame with 1041 rows and 19 variables
+NULL
+
+#' Isoelectric poit of a set of proteins from PID-DB
+#'
+#' This dataset contains more than 1000 proteins with the experimental isoelectric point and the predicted pI using different methods.
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item PROTEIN. The potein accession
+#'   \item EXPERIMENTAL. The experimental isoelectric point
+#'   \item COFACTOR. The isoelectric point using the method developed by Cargile and co-workers.
+#'   \item ITERATIVE_THURLKILL
+#'   \item ITERATIVE_TOSELAND
+#'   \item ITERATIVE_GRIMSLEY
+#'   \item ITERATIVE_SOLOMON
+#'   \item ITERATIVE_EMBOSS
+#'   \item ITERATIVE_LEHNINGER
+#'   \item ITERATIVE_PATRICKIOS
+#'   \item ITERATIVE_RICHARD
+#'   \item ITERATIVE_RODWELL
+#'   \item ITERATIVE_SILLERO
+#'   \item BRANCA
+#'   \item BJELL_DEFAULT
+#'   \item BJELL_SKOOG
+#'   \item BJELL_CALLIBRATED
+#'   \item BJELL_EXPASY
+#'   \item SVM
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name proteins
+#' @usage data(proteins)
+#' @format A data frame with 1041 rows and 19 variables
+NULL
+
+
